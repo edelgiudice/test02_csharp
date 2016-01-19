3af8f51db14b4e4fa994a750914bbfada109c863
edelgiudice <emilio.delgiudicegmail.com>
2016-01-19 20:17:31 +0000

    First commit

---
+++ HRApplicationTool/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HRApplicationTool.Startup))]
+namespace HRApplicationTool
+{
+    public partial class Startup
+    {
+        // Config
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
