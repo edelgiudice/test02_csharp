2e1cef48973a0336bc5d5c608ff4232a13b2f7dc
edelgiudice <emilio.delgiudicegmail.com>
2016-01-19 22:40:28 +0000

    Identity Implementation step 1

---
+++ HRApplicationTool/Migrations/Configuration.cs
@@ -0,0 +1,62 @@
+namespace HRApplicationTool.Migrations
+{
+    using HRApplicationTool.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HRApplicationTool.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "HRApplicationTool.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(HRApplicationTool.Models.ApplicationDbContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                // Add missing roles
+                var role = roleManager.FindByName("Admin");
+                if (role == null)
+                {
+                    role = new IdentityRole("Admin");
+                    roleManager.Create(role);
+                }
+
+                // Create test users
+                var user = userManager.FindByName("admin");
+                if (user == null)
+                {
+                    var newUser = new ApplicationUser()
+                    {
+                        UserName = "admin",
+                    };
+                    userManager.Create(newUser, "Password1!");
+                    userManager.AddToRole(newUser.Id, "Admin");
+                }
+            }
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
