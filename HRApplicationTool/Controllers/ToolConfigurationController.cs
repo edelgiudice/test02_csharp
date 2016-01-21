9afd74d88df9c282fc19e824ddfc453f360bf207
edelgiudice <emilio.delgiudicegmail.com>
2016-01-21 04:11:10 +0000

    final

--- HRApplicationTool/Controllers/ToolConfigurationController.cs
+++ HRApplicationTool/Controllers/ToolConfigurationController.cs
@@ -1,9 +1,11 @@
-﻿using HRApplicationTool.Models;
+﻿using Hangfire;
+using HRApplicationTool.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,15 +19,23 @@ namespace HRApplicationTool.Controllers
         { 
             return View(ToolConfiguration.Instance); 
         }
-        
+
         public ActionResult SendReport()
         {
+            BackgroundJob.Enqueue(() => SendEmailReport());
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        public static void SendEmailReport()
+        {
+
             var fromAddress = new MailAddress("edguktest@gmail.com");
             var fromPassword = "ed220709";
             var toAddress = new MailAddress("emilio.delgiudice@gmail.com");
 
-            string subject = "subject";
-            string body = "body";
+            string subject = "Report";
+            
 
             System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient
             {
@@ -41,14 +51,37 @@ namespace HRApplicationTool.Controllers
             using (var message = new MailMessage(fromAddress, toAddress)
             {
                 Subject = subject,
-                Body = body
+                Body = reportBody()
             })
 
 
-            smtp.Send(message);
-           
-            return RedirectToAction("Index","Home");
+                smtp.Send(message);
+
         }
+
+        private static string reportBody()
+        {
+            StringBuilder body = new StringBuilder();
+            using(var dbContext = new ApplicationDbContext())
+            {
+            
+                StringBuilder header = new StringBuilder();
+                header.Append("Name, Surname, Address, ");
+               
+                var skills = dbContext.SkillModels.ToList();
+                header.Append(skills.OrderBy(o => o.SkillID).Select(s => s.SkillName).Aggregate((current, next) => current + ", " + next));
+                body.AppendLine(header.ToString());
+                foreach(var application in dbContext.ApplicationModels.ToList().Where(w => w.RegistrationTime>DateTime.Now.Subtract(TimeSpan.FromDays(1))))
+                {
+                    var ApplicationStr = new StringBuilder();
+                    ApplicationStr.AppendFormat("{0}, {1}, {2}", application.FirstName, application.Surname, application.Address);
+                    body.AppendLine(ApplicationStr.ToString());
+                }
+                
+             }
+            return body.ToString();
+        }
+
         //
         // GET: /ToolConfiguration/Edit/5
         public ActionResult Edit()
--- HRApplicationTool/Startup.cs
+++ HRApplicationTool/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Owin;
+﻿using Hangfire;
+using HRApplicationTool.Controllers;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(HRApplicationTool.Startup))]
@@ -10,6 +12,13 @@ namespace HRApplicationTool
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            GlobalConfiguration.Configuration
+               .UseSqlServerStorage("DefaultConnection");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+
+            RecurringJob.AddOrUpdate("report",() => ToolConfigurationController.SendEmailReport(), Cron.Daily(13,00));
         }
     }
 }
